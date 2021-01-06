cb7a528cd70aefaff3bf38420b0a44f0bb3c6c53
GiantLuigi4 <tjmferet@hotmail.com>
2021-01-06 18:27:25 -0500

    formatting

--- src/main/java/net/minecraft/registry/BlockRegistry.java
+++ src/main/java/net/minecraft/registry/BlockRegistry.java
@@ -1,10 +1,17 @@
 package net.minecraft.registry;
 public class BlockRegistry {
 	//Methods
-	public static net.minecraft.world.blocks.Block register(java.lang.String string, net.minecraft.world.blocks.Block block){return null;}
-
-
+	public static net.minecraft.world.blocks.Block register(java.lang.String string, net.minecraft.world.blocks.Block block) {
+		return null;
+	}
+	
+	
 	//Fields
-	public static net.minecraft.world.blocks.Block getAir(){return null;}
-	public static net.minecraft.world.blocks.Block getStone(){return null;}
+	public static net.minecraft.world.blocks.Block getAir() {
+		return null;
+	}
+	
+	public static net.minecraft.world.blocks.Block getStone() {
+		return null;
+	}
 }
