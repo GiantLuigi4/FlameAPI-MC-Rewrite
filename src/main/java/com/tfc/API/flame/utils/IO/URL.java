c3141f4ecd89ec4acfd58009eaddded95993d7bc
GiantLuigi4 <tjmferet@hotmail.com>
2021-01-06 20:53:48 -0500

    Flame mappings helper

---
+++ src/main/java/com/tfc/API/flame/utils/IO/URL.java
@@ -0,0 +1,26 @@
+package com.tfc.API.flame.utils.IO;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class URL {
+	public static String readUrl(String urlString) throws IOException {
+		BufferedReader reader = null;
+		try {
+			java.net.URL url = new java.net.URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuilder builder = new StringBuilder();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1)
+				builder.append(chars, 0, read);
+			
+			return builder.toString();
+			
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+}
---
+++ src/main/java/com/tfc/API/flamemc/utils/mapping/Flame.java
@@ -0,0 +1,46 @@
+package com.tfc.API.flamemc.utils.mapping;
+
+import com.tfc.API.flamemc.GameInstance;
+import com.tfc.mappings.structure.Class;
+import com.tfc.mappings.structure.FlameMapHolder;
+
+import static com.tfc.API.flame.utils.IO.URL.readUrl;
+
+public class Flame {
+	public static final FlameMapHolder holder;
+	
+	static {
+		try {
+			holder = new FlameMapHolder(readUrl("https://raw.githubusercontent.com/GiantLuigi4/FlameAPI-MC-Rewrite/master/mappings/flame_mappings.mappings"));
+		} catch (Throwable err) {
+			throw new RuntimeException(err);
+		}
+	}
+	
+	public Class getFromObsf(String obsf) {
+		return getFromObsf(GameInstance.INSTANCE.versionMap, obsf);
+	}
+	
+	public Class getFromObsf(String version, String obsf) {
+		Class clazz = Intermediary.getClassFromObsf(version, obsf);
+		if (clazz != null) {
+			Class clazz2 = Intermediary.getClassFromInter("1.16.4", clazz.getSecondaryName());
+			if (clazz2 != null) {
+				return holder.getFromPrimaryName(clazz2.getPrimaryName());
+			}
+		}
+		return null;
+	}
+	
+	public Class getFromMapped(String version, String mapped) {
+		{
+			Class clazz = Mojmap.getClassFromMojmap(version, mapped);
+			if (clazz != null) return getFromObsf(version, clazz.getSecondaryName());
+			else return getFromObsf(version, Intermediary.getClassFromInter(version, mapped).getSecondaryName());
+		}
+	}
+	
+	public Class getFromMapped(String mapped) {
+		return getFromMapped(GameInstance.INSTANCE.versionMap, mapped);
+	}
+}
