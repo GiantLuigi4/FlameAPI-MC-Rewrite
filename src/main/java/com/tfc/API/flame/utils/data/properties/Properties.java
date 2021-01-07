1c8a65b131195a5d93513b39e4d228c8385405f2
GiantLuigi4 <tjmferet@hotmail.com>
2021-01-06 22:02:46 -0500

    start working on runtime class gen... will "finish" tomorrow

---
+++ src/main/java/com/tfc/API/flame/utils/data/properties/Properties.java
@@ -0,0 +1,114 @@
+package com.tfc.API.flame.utils.data.properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Properties {
+	private final HashMap<String, String> properties = new HashMap<>();
+	
+	public Properties(File toParse) throws IOException {
+		FileInputStream stream = new FileInputStream(toParse);
+		byte[] bytes = new byte[stream.available()];
+		stream.read(bytes);
+		stream.close();
+		String prop = new String(bytes);
+		String[] properties = prop.split("\n");
+		for (String s : properties) {
+			if (s.contains(":")) {
+				String[] split = s.split(":", 2);
+				this.properties.put(split[0].trim(), split[1].trim());
+			}
+			if (s.contains("=")) {
+				String[] split = s.split("=", 2);
+				this.properties.put(split[0].trim(), split[1].trim());
+			}
+		}
+	}
+	
+	public Properties(String toParse) {
+		String[] properties = toParse.split("\n");
+		for (String s : properties) {
+			if (s.contains(":")) {
+				String[] split = s.split(":", 2);
+				this.properties.put(split[0].trim(), split[1].trim());
+			}
+			if (s.contains("=")) {
+				String[] split = s.split("=", 2);
+				this.properties.put(split[0].trim(), split[1].trim());
+			}
+		}
+	}
+	
+	public static Properties parse(Object object) {
+		Properties properties = new Properties("");
+		Field[] fields = object.getClass().getFields();
+		for (Field f : fields) {
+			try {
+				properties.addValue(f.getName(), f.get(object).toString());
+			} catch (Throwable ignored) {
+			}
+		}
+		return properties;
+	}
+	
+	public static Properties parse(Object object, String file) {
+		try {
+			Properties properties = new Properties(new File(file));
+			Field[] fields = object.getClass().getFields();
+			for (Field f : fields) {
+				try {
+					properties.addValue(f.getName(), f.get(object).toString());
+					Object val = properties.getValue(f.getName());
+					if (f.getType().equals(int.class)) val = Integer.valueOf((String) val);
+					else if (f.getType().equals(boolean.class)) val = Boolean.valueOf((String) val);
+					else if (f.getType().equals(double.class)) val = Double.valueOf((String) val);
+					else if (f.getType().equals(float.class)) val = Float.valueOf((String) val);
+					else if (f.getType().equals(Long.class)) val = Long.valueOf((String) val);
+					else if (f.getType().equals(Byte.class)) val = Byte.valueOf((String) val);
+					f.set(object, val);
+				} catch (Throwable ignored) {
+				}
+			}
+			return properties;
+		} catch (Throwable ignored) {
+			return parse(object);
+		}
+	}
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		properties.forEach((name, val) -> builder.append(name).append(":").append(val).append("\n"));
+		return builder.toString();
+	}
+	
+	public String getValue(String property) {
+		if (properties.containsKey(property))
+			return properties.get(property);
+		return null;
+	}
+	
+	public <A extends java.io.Serializable> A getValue(String property, Class<A> aClass) {
+		try {
+			return (A) aClass.getMethod("valueOf").invoke(null, properties.get(property));
+		} catch (Throwable ignored) {
+			return null;
+		}
+	}
+	
+	public void setValue(String key, String value) {
+		if (properties.containsKey(key)) properties.replace(key, value);
+		else properties.put(key, value);
+	}
+	
+	public void addValue(String key, String value) {
+		if (!properties.containsKey(key)) properties.put(key, value);
+	}
+	
+	public Set<String> getEntries() {
+		return properties.keySet();
+	}
+}
