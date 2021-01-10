40f330dfb80cd93234d2ade5f6ddd689c33692f6
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2021-01-10 22:56:22 +0100

    fixed duplication

--- src/main/java/com/tfc/flame/API/utils/IO/FileUtils.java
+++ src/main/java/com/tfc/flame/API/utils/IO/FileUtils.java
@@ -7,17 +7,7 @@ import java.io.FileOutputStream;
 
 public class FileUtils {
 	public static void write(File file, String text) {
-		try {
-			if (FlameLauncher.isDev) file = new File("run/" + file);
-			if (!file.exists()) {
-				file.getParentFile().mkdirs();
-				file.createNewFile();
-			}
-			FileOutputStream stream = new FileOutputStream(file);
-			stream.write(text.getBytes());
-			stream.close();
-		} catch (Throwable ignored) {
-		}
+		write(file, text.getBytes());
 	}
 	
 	public static void write(File file, byte[] bytes) {
